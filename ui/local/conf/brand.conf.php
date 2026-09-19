87fd674d6d68ce150e334d86c733dbd780949300
clikhar <84700563+clikhar@users.noreply.github.com>
2026-09-19 14:07:30 +0530

    Configure NGP Central Railway Zabbix branding

---
+++ ui/local/conf/brand.conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * NGP Division, Central Railway - Zabbix branding.
+ *
+ * Custom branding is loaded by Zabbix's CBrandHelper.
+ */
+return [
+    'BRAND_LOGO' => '/rebranding/indian-railways-main.svg',
+    'BRAND_LOGO_SIDEBAR' => '/rebranding/indian-railways-sidebar.svg',
+    'BRAND_LOGO_SIDEBAR_COMPACT' => '/rebranding/indian-railways-compact.svg',
+    'BRAND_FOOTER' => 'NGP Division, Central Railway'
+];
